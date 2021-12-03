a9eea0fb4029acc1de349be0d30e08d921e1fa54
Alisha <irl_a@mail.ru>
2021-12-03 16:36:24 +0300

    fixed

--- src/StreamService/Program.cs
+++ src/StreamService/Program.cs
@@ -7,53 +7,53 @@ using System;
 namespace LT.DigitalOffice.StreamService
 {
   public class Program
+  {
+    public static void Main(string[] args)
     {
-      public static void Main(string[] args)
+      var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+      string seqServerUrl = Environment.GetEnvironmentVariable("seqServerUrl");
+      if (string.IsNullOrEmpty(seqServerUrl))
       {
-        var configuration = new ConfigurationBuilder()
-          .AddJsonFile("appsettings.json")
-          .Build();
-
-        string seqServerUrl = Environment.GetEnvironmentVariable("seqServerUrl");
-        if (string.IsNullOrEmpty(seqServerUrl))
-        {
-          seqServerUrl = configuration["Serilog:WriteTo:1:Args:serverUrl"];
-        }
-
-        string seqApiKey = Environment.GetEnvironmentVariable("seqApiKey");
-        if (string.IsNullOrEmpty(seqApiKey))
-        {
-          seqApiKey = configuration["Serilog:WriteTo:1:Args:apiKey"];
-        }
-
-        Log.Logger = new LoggerConfiguration().ReadFrom
-          .Configuration(configuration)
-          .Enrich.WithProperty("Service", "StreamService")
-          .WriteTo.Seq(
-              serverUrl: seqServerUrl,
-              apiKey: seqApiKey)
-          .CreateLogger();
-
-        try
-        {
-          CreateHostBuilder(args).Build().Run();
-        }
-        catch (Exception exc)
-        {
-          Log.Fatal(exc, "Can not properly start StreamService.");
-        }
-        finally
-        {
-          Log.CloseAndFlush();
-        }
+        seqServerUrl = configuration["Serilog:WriteTo:1:Args:serverUrl"];
       }
 
-      public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-          .UseSerilog()
-          .ConfigureWebHostDefaults(webBuilder =>
-          {
-            webBuilder.UseStartup<Startup>();
-          });
+      string seqApiKey = Environment.GetEnvironmentVariable("seqApiKey");
+      if (string.IsNullOrEmpty(seqApiKey))
+      {
+        seqApiKey = configuration["Serilog:WriteTo:1:Args:apiKey"];
+      }
+
+      Log.Logger = new LoggerConfiguration().ReadFrom
+        .Configuration(configuration)
+        .Enrich.WithProperty("Service", "StreamService")
+        .WriteTo.Seq(
+            serverUrl: seqServerUrl,
+            apiKey: seqApiKey)
+        .CreateLogger();
+
+      try
+      {
+        CreateHostBuilder(args).Build().Run();
+      }
+      catch (Exception exc)
+      {
+        Log.Fatal(exc, "Can not properly start StreamService.");
+      }
+      finally
+      {
+        Log.CloseAndFlush();
+      }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+      Host.CreateDefaultBuilder(args)
+        .UseSerilog()
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseStartup<Startup>();
+        });
   }
 }
